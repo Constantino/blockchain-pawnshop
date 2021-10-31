975c8122351f0567138b8bca91a092800a1dd039
Constantino Mora <5051080+Constantino@users.noreply.github.com>
2021-10-31 15:43:11 -0600

    Initial commit

---
+++ frontend/ethereum-boilerplate/src/hooks/useTokenPrice.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { useMoralis, useMoralisWeb3Api } from "react-moralis";
+import { c2, tokenValueTxt } from "../helpers/formatters";
+
+const useTokenPrice = (options) => {
+  const { token } = useMoralisWeb3Api();
+  const { isInitialized } = useMoralis();
+  const [tokenPrice, setTokenPrice] = useState();
+
+  useEffect(() => {
+    if (isInitialized)
+      fetchTokenPrice(options)
+        .then((price) => {
+          price.usdPrice = c2.format(price.usdPrice);
+          const { value, decimals, symbol } = price.nativePrice;
+          price.nativePrice = tokenValueTxt(value, decimals, symbol);
+          setTokenPrice(price);
+        })
+        .catch((e) => alert(e.message));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isInitialized]);
+
+  const fetchTokenPrice = async (options) => {
+    const { chain, address } = options;
+    return await token
+      .getTokenPrice({ chain, address })
+      .then((result) => result)
+      .catch((e) => alert(e.message));
+  };
+  return { fetchTokenPrice, tokenPrice };
+};
+
+export default useTokenPrice;
