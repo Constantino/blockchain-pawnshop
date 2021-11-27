d1e3cbf68551b1bb758a1322ead2b7de09f18407
Constantino Mora <5051080+Constantino@users.noreply.github.com>
2021-11-26 23:37:56 -0600

    Fixed NFTMinter contract to use openzeppelin library instead of http imports

--- contracts/NFTMinter.sol
+++ contracts/NFTMinter.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.1;
 
-    import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol';
-    import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol';
+    import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+    import "@openzeppelin/contracts/access/Ownable.sol";
 
     contract Example is ERC721, Ownable {
         
